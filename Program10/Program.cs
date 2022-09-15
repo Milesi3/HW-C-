b2136d984055424479f3cd7522f33c1fd703bd41
milesi <irina-rxv@yandex.ru>
2022-09-15 16:06:03 +0300

    Сделал вывод массива через цикл который ищет значения меньше или равно 3 символам

--- Program10/Program.cs
+++ Program10/Program.cs
@@ -3,5 +3,11 @@
 Console.Write("Введите через пробел: ");
 string temp = Console.ReadLine();
 string[] array = temp.Split(' ');
-
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " "); 
+    }
+}
     
